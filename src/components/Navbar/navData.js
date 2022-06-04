da54292b07b36388ad73ee16d758395cb1ac236b
Mark <marusmark2@gmail.com>
2022-06-04 12:02:09 +0200

    Init

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,36 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Information',
+    pathname: '/#details',
+  },
+  {
+    label: 'BuyMeACoffe',
+    pathname: 'https://www.buymeacoffee.com/cobaltdevelop',
+  },
+  {
+    label: 'Developers',
+    pathname: 'https://linktr.ee/cobaltdevelopers',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/marusisgod',
+    icon: 'twitter',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/MarkMarus',
+    icon: 'github',
+  },
+  {
+    label: 'Telegram',
+    url: 'https://t.me/CobaltDev',
+    icon: 'telegram'
+  }
+];
---
+++ src/layouts/Home/Profile.js
@@ -0,0 +1,114 @@
+import profileKatakana from 'assets/katakana-profile.svg?url';
+import profileImgLarge from 'assets/profile-large.jpg';
+import profileImgPlaceholder from 'assets/profile-placeholder.jpg';
+import profileImg from 'assets/profile.png';
+import { Button } from 'components/Button';
+import { DecoderText } from 'components/DecoderText';
+import { Divider } from 'components/Divider';
+import { Heading } from 'components/Heading';
+import { Image } from 'components/Image';
+import { Link } from 'components/Link';
+import { Section } from 'components/Section';
+import { Text } from 'components/Text';
+import { Transition } from 'components/Transition';
+import { Fragment, useState } from 'react';
+import { media } from 'utils/style';
+import styles from './Profile.module.css';
+
+const ProfileText = ({ visible, titleId }) => (
+  <Fragment>
+    <Heading className={styles.title} data-visible={visible} level={3} id={titleId}>
+      <DecoderText text="Hi there" start={visible} delay={500} />
+    </Heading>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      We are Cobalt, a web-development team currently we are located in Poland working with customers. To contact us You can use 
+      <Link href="https://www.t.me/CobaltDev"> Telegram</Link>. We offer UX design, UI
+      animations, and Development. We have a small team of proffesional and motivated developers. If you’re interested in the tools and software We
+      use or You want to know more about us check out our <Link href="https://linktr.ee/cobaltdevelopers">linktree page</Link>.
+    </Text>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      Who we are? We are young year old developers that live in code. Our goal is to make a contribution in development of worldwide-web. If we impressed you and
+      you want to help us growind you can always tip us at{' '}
+      <Link href="https://www.buymeacoffee.com/cobaltdevelop">BuyMeACoffe</Link>. Here is more <Link href="https://linktr.ee/cobaltdevelopers">about us</Link>
+    </Text>
+  </Fragment>
+);
+
+export const Profile = ({ id, visible, sectionRef }) => {
+  const [focused, setFocused] = useState(false);
+  const titleId = `${id}-title`;
+
+  return (
+    <Section
+      className={styles.profile}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      as="section"
+      id={id}
+      ref={sectionRef}
+      aria-labelledby={titleId}
+      tabIndex={-1}
+    >
+      <Transition in={visible || focused} timeout={0}>
+        {visible => (
+          <div className={styles.content}>
+            <div className={styles.column}>
+              <ProfileText visible={visible} titleId={titleId} />
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                href="https://t.me/CobaltDev"
+                icon="send"
+              >
+                Contact us via Telegram
+              </Button>
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                href="https://mail.google.com/mail/?view=cm&source=mailto&to=cobaltdevelopmentbusiness@gmail.com"
+                icon="send"
+              >
+                Email us
+              </Button>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.tag} aria-hidden>
+                <Divider
+                  notchWidth="64px"
+                  notchHeight="8px"
+                  collapsed={!visible}
+                  collapseDelay={1000}
+                />
+                <div className={styles.tagText} data-visible={visible}>
+                  About Us
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image
+                  reveal
+                  delay={100}
+                  placeholder={profileImgPlaceholder}
+                  srcSet={[profileImg]}
+                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                  alt="Me standing in front of the Torii on Miyajima, an island off the coast of Hiroshima in Japan"
+                />
+                <svg
+                  aria-hidden="true"
+                  width="135"
+                  height="765"
+                  viewBox="0 0 135 765"
+                  className={styles.svg}
+                  data-visible={visible}
+                >
+                  <use href={`${profileKatakana}#katakana-profile`} />
+                </svg>
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </Section>
+  );
+};
